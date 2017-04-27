da072e2854f4f01d127b49ac680ac076243a33a6
Franky <yudr1996@outlook.com>
2017-04-27 20:43:25 +0800

    实现串口读取并在matlab中转为数组

--- serial_connection.m
+++ serial_connection.m
@@ -1,19 +1,23 @@
 clear
 clc
 
-s = serial('COM3');  %定义串口对象
+s = serial('COM4');  %定义串口对象
 set(s,'BaudRate',9600);  %设置波特率s
 fopen(s);  %打开串口对象s
 
 interval = 100;  %这里开始的代码很Part1里的代码类似
-passo = 1;
-t = 1;
-x = 0;
-while(t<interval)
-    b = fgetl(s) %用函数fget(s)从缓冲区读取串口数据，当出现终止符（换行符）停止。
-    
-end
-fclose(s);  %关闭串口对象s
+
+fgetl(s)
+A=[str2num(fgetl(s));str2num(fgetl(s));str2num(fgetl(s))]
+
+B=A+[1;2;3]
+% while(fgetl(s))
+%     A=[fgetl(s),fgetl(s),fgetl(s)]
+%     B=[fgetl(s),fgetl(s),fgetl(s)]
+%     C=[fgetl(s),fgetl(s),fgetl(s)]
+% %     plot(real(b),imag(b),'ro');
+% end
+% fclose(s);  %关闭串口对象s
 
 % clear
 % clc
@@ -30,4 +34,4 @@ fclose(s);  %关闭串口对象s
 %     drawnow;
 % end
 % 
-% fclose(s); 
+fclose(s)
--- sketch_apr07a/sketch_apr07a.ino
+++ sketch_apr07a/sketch_apr07a.ino
@@ -9,5 +9,20 @@ void setup()
 void loop()
 {
   val = analogRead(analogPin);
-  Serial.println("1+2*i");
+  Serial.println("\\");
+/*  Serial.print("\[");
+  Serial.print("1");
+  Serial.print("\,");
+  Serial.print("2");
+  Serial.print("\,");
+  Serial.print("5");
+  Serial.println("\]");*/
+  Serial.println("2");
+  Serial.println("3");
+  Serial.println("2");
+  Serial.println("8");
+  Serial.println("5");
+  Serial.println("4");
+  Serial.println("5");
+  Serial.println("3");
 }
