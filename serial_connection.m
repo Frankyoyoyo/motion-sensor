4089799c5ae2904dc9f3ca377234899417614676
Frank <yudr1996@outlook.com>
2017-06-03 21:33:19 +0800

    完成加速度校准

--- serial_connection.m
+++ serial_connection.m
@@ -5,32 +5,52 @@ s = serial('COM3');  %定义串口对象
 set(s,'BaudRate',9600);  %设置波特率s
 fopen(s);  %打开串口对象s
 
-global A G M q0 q1 q2 q3 X Y Z T halfT Vx Vy Vz g t
-% T=0.008;halfT=0.004;
-q0=1;q1=0;q2=0;q3=0;                %4 parameters of the quaternion a,b,c,d,(v=q0+q1i+q2j+q3k)
-Vx=0;Vy=0;Vz=0;              %initial velocity
-X=0;Y=0;Z=0;                %initial position
+global A G M q0 q1 q2 q3 T halfT  g t
+q0=1;q1=0;q2=0;q3=0;                  %4 parameters of the quaternion a,b,c,d,(v=q0+q1i+q2j+q3k)
+V_static=[0; 0; 0];                            %initial velocity
+R_static=[0; 0; 0];                          %initial position
 g=9.81;
-i=0;t=0;                    %for timer usage
 T=t;
 halfT=t/2;
+i=0;t=0;                              %for timer usage
+A0=zeros(3,300);A1=zeros(3,1);        %for correction
+
+N=300;                                %设置校准矩阵长度
 while(1)
     if(str2double(fgetl(s))~=0)
         continue
     else
-        A=[str2double(fgetl(s)),str2double(fgetl(s)),str2double(fgetl(s))];
-        G=[str2double(fgetl(s)),str2double(fgetl(s)),str2double(fgetl(s))];
-        M=[str2double(fgetl(s)),str2double(fgetl(s)),str2double(fgetl(s))];
+%main part=================================================================
+    A=[str2double(fgetl(s)),str2double(fgetl(s)),str2double(fgetl(s))];
+    G=[str2double(fgetl(s)),str2double(fgetl(s)),str2double(fgetl(s))];
+    M=[str2double(fgetl(s)),str2double(fgetl(s)),str2double(fgetl(s))];
         coordinate_transformation
 %         [yaw, pitch, roll] = quat2angle([q0 q1 q2 q3])
         
-        vector_transformation
-        Ax
-        Ay
-        Az
-     
-    end
+%         vector_transformation
+    C=[(q0^2+q1^2-q2^2-q3^2),2*(q1*q2-q0*q3),2*(q1*q3+q0*q2);
+        2*(q1*q2+q0*q3),(q0^2-q1^2+q2^2-q3^2),2*(q2*q3-q0*q1);
+        2*(q1*q3-q0*q2),2*(q2*q3+q0*q1),(q0^2-q1^2-q2^2+q3^2)];
+    Acc=C*A'*g;                %加速度坐标系变换
+%get correction with 3*N data----------------------
+    if i<N
+        A0(1,i+1)=(Acc(1,1));
+        A0(2,i+1)=(Acc(2,1));
+        A0(3,i+1)=(Acc(3,1));
+    elseif i==N
+        A1(1,1)=1/N*sum(A0(1,:));
+        A1(2,1)=1/N*sum(A0(2,:));
+        A1(3,1)=1/N*sum(A0(3,:));
+    else  
+        A_static=Acc-A1    %静止坐标系三轴加速度
+        V_static=V_static+A_static*T;               %三轴速度
+        R_static=R_static+V_static*T+(1/2)*A_static*T^2;        %三轴位移
+    end    
     
+
+    
+    
+    end
 %timer--------------------------------t为每次计算所用时间
     if(i==0)
         tic
@@ -41,8 +61,8 @@ while(1)
         t=toc;
         tic;
     end
-    i=i+1;
 %-------------------------------------
+    i=i+1;
 end
 
 fclose(s)
