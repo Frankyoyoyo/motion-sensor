022ed66c3d558c415069943f930c6e4101338726
Frank <yudr1996@outlook.com>
2017-06-12 03:17:49 +0800

    暂停调试delay时间

--- test_MPU9250_NRF24L01/test_MPU9250_NRF24l01.ino
+++ test_MPU9250_NRF24L01/test_MPU9250_NRF24l01.ino
@@ -6,14 +6,12 @@
 #include <FaBo9Axis_MPU9250.h>
 FaBo9Axis fabo_9axis;
 int k;
-int i=0;
+int i = 0;
 float value[7] = {0};
-float A[3][20]={0};
-float G[3][20]={0};
-double pi=3.1415926;
+double pi = 3.1415926;
 
 void setup()
-{ 
+{
   fabo_9axis.begin();
   Mirf.spi = &MirfHardwareSpi;
   Mirf.init();
@@ -23,51 +21,38 @@ void setup()
   Mirf.config();//设置所用信道
   Serial.begin(9600);
 }
-
+float a;
+float b;
+float a0,a1,a2,g0,g1,g2;
 void loop()
 {
+  a = millis();
   int j;
-  i=i+1;
-  float ax,ay,az;
-  float gx,gy,gz;
-  
-  if((i%20)!=0)
+//  i = i + 1;
+  float ax, ay, az;
+  float gx, gy, gz;
+  a0=a1=a2=g0=g1=g2=0;
+  for (j = 0; j < 10; j++)
   {
-    fabo_9axis.readAccelXYZ(&ax,&ay,&az);
-    fabo_9axis.readGyroXYZ(&gx,&gy,&gz);
-    A[0][(i%20)-1]=ax;
-    A[1][(i%20)-1]=ay;
-    A[2][(i%20)-1]=az;
-    G[0][(i%20)-1]=gx;
-    G[1][(i%20)-1]=gy;
-    G[2][(i%20)-1]=gz;
-  }else{
-    fabo_9axis.readAccelXYZ(&ax,&ay,&az);
-    fabo_9axis.readGyroXYZ(&gx,&gy,&gz);
-    A[0][19]=ax;
-    A[1][19]=ay;
-    A[2][19]=az;
-    G[0][19]=gx;
-    G[1][19]=gy;
-    G[2][19]=gz;
-    ax=ay=az=gx=gy=gz=0.0f;
+    fabo_9axis.readAccelXYZ(&ax, &ay, &az);
+    fabo_9axis.readGyroXYZ(&gx, &gy, &gz);
 
-    for(j=0;j<20;j++){
-      ax=ax+A[0][j];
-      ay=ay+A[1][j];
-      az=az+A[2][j];
-      gx=gx+G[0][j];
-      gy=gy+G[1][j];
-      gz=gz+G[2][j];
-    }
-    ax=ax/20;
-    ay=ay/20;
-    az=az/20;
-    gx=gx/3600*pi;
-    gy=gy/3600*pi;
-    gz=gz/3600*pi;
+    a0 = a0 + ax;
+    a1 = a1 + ay;
+    a2 = a2 + az;
+    g0 = g0 + gx;
+    g1 = g1 + gy;
+    g2 = g2 + gz;
+  }
 
-   /* Serial.println("100");
+  ax = a0 / 10;
+  ay = a1 / 10;
+  az = a2 / 10;
+  gx = g0 / 1800 * pi;
+  gy = g1 / 1800 * pi;
+  gz = g2 / 1800 * pi;
+
+  /* Serial.println("100");
     Serial.println(ax);
     Serial.println(ay);
     Serial.println(az);
@@ -75,16 +60,23 @@ void loop()
     Serial.println(gy);
     Serial.println(gz);*/
 
-    value[0] = 100 ;
-    value[1] = ax;
-    value[2] = ay;
-    value[3] = az;
-    value[4] = gx;
-    value[5] = gy;
-    value[6] = gz;
-    for (k = 0; k < 7; k++) {
-      Mirf.send((byte *)&value[i]);
-      while (Mirf.isSending()) {};
-    }
-  }
+  value[0] = 100;
+  value[1] = ax;
+  value[2] = ay;
+  value[3] = az;
+  value[4] = gx;
+  value[5] = gy;
+  value[6] = gz;
+
+//  b = millis();     20ms
+  
+  for (k = 0; k < 7; k++) {
+   Mirf.send((byte *)&value[i]);
+   while (Mirf.isSending()) {};
+      delay(200);
+  }                                     //40ms
+
+//  b = millis();
+//  b = b - a;
+//  Serial.println(b);
 }
