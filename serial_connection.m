5bcfb9dc91ba238a8fd6e09ed0c6980ec5864585
Frank <yudr1996@outlook.com>
2017-06-12 01:36:24 +0800

    debug

--- serial_connection.m
+++ serial_connection.m
@@ -1,7 +1,7 @@
 clear
 clc
 
-s = serial('COM7');                   %define serial port
+s = serial('COM5');                   %define serial port
 set(s,'BaudRate',9600);               %set baud rate
 fopen(s);                             %open serial port s
 %==============================================================
@@ -17,12 +17,10 @@ g=9.81;
 halfT=T/2;
 i=0;T=0;                              %for timer usage
 A0=zeros(3,N);A1=zeros(3,1);          %for correction
-G0=zeros(N,3);G1=zeros(1,3);          %0-collect data£¬1-correctiong data
-
-T0=zeros(1,N);
+G0=zeros(N,3);G1=zeros(1,3);          %0-collect data??1-correctiong data
 
 
-while(i<2000)
+while(i<1500)
     if(str2double(fgetl(s))~=100)
         continue
     else
@@ -33,9 +31,9 @@ while(i<2000)
         
     coordinate_transformation
     %------------------------
-    if i>N
+    if i>2*N
         for k=1:3
-            if abs(G(1,k))<=0.01
+            if abs(G(1,k))<=0.02
                 G(1,k)=0;
             end
         end
@@ -54,12 +52,10 @@ while(i<2000)
             G0(i+1,1)=(G(1,1));
             G0(i+1,2)=(G(1,2));
             G0(i+1,3)=(G(1,3));
-            
-            if i>0
-                T0(1,i)=T;
-            end
-            
-        elseif i==N                                          %correcting algorithm,T£¨average value£©
+        elseif i==N                                          %correcting algorithm?¡§curve fitting/average value??
+%             G1(1,1)=polyfit((1:N),G0(:,1)',0);
+%             G1(1,2)=polyfit((1:N),G0(:,2)',0);
+%             G1(1,3)=polyfit((1:N),G0(:,3)',0);
             G1(1,1)=1/N*sum(G0(:,1));
             G1(1,2)=1/N*sum(G0(:,2));
             G1(1,3)=1/N*sum(G0(:,3));
@@ -67,7 +63,6 @@ while(i<2000)
             A0(1,i-N+1)=(Acc(1,1));
             A0(2,i-N+1)=(Acc(2,1));
             A0(3,i-N+1)=(Acc(3,1));
-            T=1/(N-1)*sum(T0(1,:))
         elseif i<(2*N)
             A0(1,i-N+1)=(Acc(1,1));
             A0(2,i-N+1)=(Acc(2,1));
@@ -77,53 +72,58 @@ while(i<2000)
             A1(2,1)=polyfit((1:N),A0(2,:),0);
             A1(3,1)=polyfit((1:N),A0(3,:),0);
         else  
-            A_static=Acc-A1;                                  %static coordinate system£ºacceleration
+            A_static=Acc-A1;                                  %static coordinate system??acceleration
             V_static=V_static+A_static*T;                     %                          velocity
         %----------------------------------
                 if abs(sum(A(1,:).^2)-1)<0.04
                     A_static=[0;0;0];
                 end
-%             for j=1:3
-%                 if abs(A_static(j,1))<0.03
-%                     V_static(j,1)=0;
-%                 end
-%             end
+            for j=1:3
+                if abs(A_static(j,1))<0.03
+                    V_static(j,1)=0;
+                end
+            end
         %----------------------------------
             R_static=R_static+V_static*T+(1/2)*A_static*T^2;  %                          displacement
         end        
     end
     %--------------timer------------------(T is the period of each calculation)
-    if i<N
-        if(i==0)
-            tic
-        elseif(rem(i,2)==0)
-            T=toc;
-            tic
-        else
-            T=toc;
-            tic;
-        end
+    if(i==0)
+        tic
+    elseif(rem(i,2)==0)
+        T=toc;
+        tic
+    else
+        T=toc;
+        tic;
     end
 %=================output area================================
     i
-%     plot3(R_static(1,1),R_static(2,1),R_static(3,1),'o');
+    A
+    T
+    
+%     plot3(R_static(1,1),R_static(2,1),R_static(3,1),'r.','MarkerSize',80);
 %     axis([-0.5 0.5 -0.5 0.5 -0.5 0.5]);
 %     drawnow
+
+%    [yaw, pitch, roll] = quat2angle([q0 q1 q2 q3]);
+   plot(i,R_static(1,1),'or')
+   hold on
+   axis([1000 1500 -1 1])
+%    title('roll(??)-i')
+   drawnow
+
 %------------debugging-----------------
-   G   
+%    G   
 % 	 A_static
 %    V_static
-    Q=[q0,q1,q2,q3]
-%     A
-    Acc
-    A_static
-   [yaw, pitch, roll] = quat2angle([q0 q1 q2 q3])
-% 
-   plot(i,A_static(3,1),'or')
-   hold on
-   axis([1000 2000 -10 10])
-%    title('roll(¡ã)-i')
-   drawnow
+%    Acc
+%    [yaw, pitch, roll] = quat2angle([q0 q1 q2 q3]);
+%    plot(i,roll/3.14159*180,'or')
+%    hold on
+%    axis([1000 2000 -120 120])
+%    title('roll(??)-i')
+%    drawnow
 
 %    V_static
 %    R_static
--- test_MPU9250/test_MPU9250.ino
+++ test_MPU9250/test_MPU9250.ino
@@ -6,38 +6,73 @@
   SCL-A5
  */
 
-#include <string.h>
+
 #include <Wire.h>
 #include <FaBo9Axis_MPU9250.h>
-#define T=0.008f
-#define N=500
-#define pi=3.1415926
-
+int i=0;
 FaBo9Axis fabo_9axis;
+float A[3][20]={0};
+float G[3][20]={0};
+double pi=3.1415926;
 
 void setup() {
   Serial.begin(9600);
+
   fabo_9axis.begin();
-  int i=0;
-  float q0,q1,q2,q3;
-  float G0[3][N]
 }
 
 void loop() {
+  int j;
+  i=i+1;
   float ax,ay,az;
   float gx,gy,gz;
-  fabo_9axis.readAccelXYZ(&ax,&ay,&az);
-  fabo_9axis.readGyroXYZ(&gx,&gy,&gz);
-  float A[3]={ax,ay,az}
 
-  gx=gx*pi/180;
-  gy=gy*pi/180;
-  gz=gz*pi/180;                    //弧度值角速度
+  if((i%20)!=0)
+  {
+    fabo_9axis.readAccelXYZ(&ax,&ay,&az);
+    fabo_9axis.readGyroXYZ(&gx,&gy,&gz);
+    A[0][(i%20)-1]=ax;
+    A[1][(i%20)-1]=ay;
+    A[2][(i%20)-1]=az;
+    G[0][(i%20)-1]=gx;
+    G[1][(i%20)-1]=gy;
+    G[2][(i%20)-1]=gz;
+  }else{
+    fabo_9axis.readAccelXYZ(&ax,&ay,&az);
+    fabo_9axis.readGyroXYZ(&gx,&gy,&gz);
+    A[0][19]=ax;
+    A[1][19]=ay;
+    A[2][19]=az;
+    G[0][19]=gx;
+    G[1][19]=gy;
+    G[2][19]=gz;
+    ax=ay=az=gx=gy=gz=0.0f;
 
-  double C={{(q0^2+q1^2-q2^2-q3^2),2*(q1*q2-q0*q3),2*(q1*q3+q0*q2)},
-            {2*(q1*q2+q0*q3),(q0^2-q1^2+q2^2-q3^2),2*(q2*q3-q0*q1)},
-            {2*(q1*q3-q0*q2),2*(q2*q3+q0*q1),(q0^2-q1^2-q2^2+q3^2)}};
-  double Acc=C*A'*g;
+    for(j=0;j<20;j++){
+      ax=ax+A[0][j];
+      ay=ay+A[1][j];
+      az=az+A[2][j];
+      gx=gx+G[0][j];
+      gy=gy+G[1][j];
+      gz=gz+G[2][j];
+    }
+    ax=ax/20;
+    ay=ay/20;
+    az=az/20;
+    gx=gx/3600*pi;
+    gy=gy/3600*pi;
+    gz=gz/3600*pi;
+   
+    Serial.println("100000");
+    Serial.println(ax);
+    Serial.println(ay);
+    Serial.println(az);
+    Serial.println(gx);
+    Serial.println(gy);
+    Serial.println(gz);
+    delay(30);
+  }
+
 }
 
 
