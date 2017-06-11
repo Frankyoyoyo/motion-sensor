525d3bafc0256d0298d91db2f8e6dd3e895fd8d6
Frank <yudr1996@outlook.com>
2017-06-11 11:43:03 +0800

    undone change

--- test_MPU9250/test_MPU9250.ino
+++ test_MPU9250/test_MPU9250.ino
@@ -6,47 +6,38 @@
   SCL-A5
  */
 
-
+#include <string.h>
 #include <Wire.h>
 #include <FaBo9Axis_MPU9250.h>
+#define T=0.008f
+#define N=500
+#define pi=3.1415926
 
 FaBo9Axis fabo_9axis;
 
 void setup() {
   Serial.begin(9600);
-
   fabo_9axis.begin();
-
+  int i=0;
+  float q0,q1,q2,q3;
+  float G0[3][N]
 }
 
 void loop() {
   float ax,ay,az;
   float gx,gy,gz;
-  float mx,my,mz;
-  float temp;
-
   fabo_9axis.readAccelXYZ(&ax,&ay,&az);
   fabo_9axis.readGyroXYZ(&gx,&gy,&gz);
-  fabo_9axis.readMagnetXYZ(&mx,&my,&mz);
-  fabo_9axis.readTemperature(&temp);
+  float A[3]={ax,ay,az}
 
-  double pi=3.1415926;
   gx=gx*pi/180;
   gy=gy*pi/180;
-  gz=gz*pi/180;
+  gz=gz*pi/180;                    //弧度值角速度
 
-  Serial.println("100");
-  Serial.println(ax);
-  Serial.println(ay);
-  Serial.println(az);
-  Serial.println(gx);
-  Serial.println(gy);
-  Serial.println(gz);
-  /*Serial.println(mx);
-  Serial.println(my);
-  Serial.println(mz);*/
-
-  //delay(1000);
+  double C={{(q0^2+q1^2-q2^2-q3^2),2*(q1*q2-q0*q3),2*(q1*q3+q0*q2)},
+            {2*(q1*q2+q0*q3),(q0^2-q1^2+q2^2-q3^2),2*(q2*q3-q0*q1)},
+            {2*(q1*q3-q0*q2),2*(q2*q3+q0*q1),(q0^2-q1^2-q2^2+q3^2)}};
+  double Acc=C*A'*g;
 }
 
 
